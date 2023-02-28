4f207dd6cc84d3e8dc88dd406cc8bdaa4f0c3563
rohan mathur <rohanmathur@rohans-MacBook-Air.local>
2023-02-28 19:54:04 +0530

    updated year in footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,7 +4,7 @@ import { AiFillInstagram, AiOutlineTwitter} from 'react-icons/ai';
 const Footer = () => {
   return (
     <div className="footer-container">
-      <p>2022 Rohan Headphones All rights reserverd</p>
+      <p>2023 Rohan Headphones All rights reserverd</p>
       <p className="icons">
         <AiFillInstagram />
         <AiOutlineTwitter />
